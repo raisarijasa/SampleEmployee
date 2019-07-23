6e8e4f37b9926aa4dd5dc3aee098c792b558ff78
Rai S. Arijasa <raisuardhyana.arijasa@mitrais.com>
2019-07-23 13:36:40 +0800

    init sample project

---
+++ src/main/java/com/test/SampleEmployee/SampleEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.test.SampleEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleEmployeeApplication.class, args);
+	}
+
+}

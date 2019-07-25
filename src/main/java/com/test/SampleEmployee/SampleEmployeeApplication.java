403ebabe29b4678d34bfe88ef9b67954a89aa17f
Rai S. Arijasa <raisuardhyana.arijasa@mitrais.com>
2019-07-25 17:43:27 +0800

    update main class

--- src/main/java/com/test/SampleEmployee/SampleEmployeeApplication.java
+++ src/main/java/com/test/SampleEmployee/SampleEmployeeApplication.java
@@ -2,9 +2,10 @@ package com.test.SampleEmployee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SampleEmployeeApplication {
+public class SampleEmployeeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleEmployeeApplication.class, args);

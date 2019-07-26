1659a8636c1da050c570617f7006ddbb9234e2d0
raisarijasa <tenget.people@gmail.com>
2019-07-26 17:56:14 +0800

    Update EmployeeRepository.java
    test webhook

--- src/main/java/com/test/SampleEmployee/repository/EmployeeRepository.java
+++ src/main/java/com/test/SampleEmployee/repository/EmployeeRepository.java
@@ -11,7 +11,7 @@ public class EmployeeRepository {
     private static List<Employee> list = new ArrayList<>();
 
     static {
-        list.add(new Employee(1, "FName1", "LName1", "user.one@email.com"));
+        list.add(new Employee(1, "FName123", "LName1", "user.one@email.com"));
         list.add(new Employee(2, "FName2", "LName2", "user.two@email.com"));
         list.add(new Employee(3, "FName3", "LName3", "user.three@email.com"));
         list.add(new Employee(4, "FName4", "LName4", "user.four@email.com"));

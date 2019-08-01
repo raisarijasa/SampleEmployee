9708443fa080006eb4c4174a5c9a619a57470d32
Rai S. Arijasa <raisuardhyana.arijasa@mitrais.com>
2019-08-01 13:15:13 +0800

    fix error coverage

--- src/main/java/com/test/sampleemployee/helper/Constant.java
+++ src/main/java/com/test/sampleemployee/helper/Constant.java
@@ -3,6 +3,4 @@ package com.test.sampleemployee.helper;
 public class Constant {
     public static final int CODE_SUCCESS = 1;
     public static final int CODE_FAILED = 0;
-
-    private Constant() {}
 }

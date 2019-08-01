76b29f22ae1c547731341c1a116c8a4ab6e0349a
Rai S. Arijasa <raisuardhyana.arijasa@mitrais.com>
2019-08-01 14:47:11 +0800

    add private constructor

--- src/main/java/com/test/sampleemployee/helper/Constant.java
+++ src/main/java/com/test/sampleemployee/helper/Constant.java
@@ -3,4 +3,7 @@ package com.test.sampleemployee.helper;
 public class Constant {
     public static final int CODE_SUCCESS = 1;
     public static final int CODE_FAILED = 0;
+
+    private Constant() {
+    }
 }

25cf20f27e0e161f8d40967b0ad4bb1cd46c8113
arijasa <56530705+arijasa@users.noreply.github.com>
2019-10-14 13:54:26 +0800

    Update EmployeeRepositoryImpl.java

--- src/main/java/com/test/sampleemployee/repository/EmployeeRepositoryImpl.java
+++ src/main/java/com/test/sampleemployee/repository/EmployeeRepositoryImpl.java
@@ -17,6 +17,7 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
         list.add(new Employee(3, "FName3", "LName3", "user.three@email.com"));
         list.add(new Employee(4, "FName4", "LName4", "user.four@email.com"));
         list.add(new Employee(5, "FName5", "LName5", "user.five@email.com"));
+        list.add(new Employee(6, "FName6", "LName6", "user.six@email.com"));
     }
 
     public List<Employee> getAllEmployees() {
